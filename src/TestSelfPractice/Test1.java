57719aea03d288b262f0b0ebc8747f177610305f
muca133 <114365351+muca133@users.noreply.github.com>
2023-01-08 13:00:24 -0500

    test1 created

---
+++ src/TestSelfPractice/Test1.java
@@ -0,0 +1,12 @@
+package TestSelfPractice;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("I am going to do same git Practice ");
+    }
+
+
+
+}
